55518c4cb531ace1a67cccc162b505f8ea145c38
Krish Bajaj <53073541+Krish-Bajaj@users.noreply.github.com>
2021-06-27 02:40:40 +0530

    feat: added code to pull contract notes from Gmail

---
+++ readers/fetch_emails.py
@@ -0,0 +1,25 @@
+from simplegmail import Gmail
+from simplegmail.query import construct_query
+import os
+import shutil
+
+
+def getContractNotes():
+    gmail = Gmail()
+
+    label_name = input("\nEnter your label under which files are stored: ")
+    query_params = {
+        "read": True,
+        "labels":[[label_name]]
+    }
+
+    messages = gmail.get_messages(query=construct_query(query_params))
+    for message in messages:
+        if message.attachments:
+            for attm in message.attachments:
+                attm.save()
+
+    # moves pdf files from current directory to emails folder
+    for file in os.listdir():
+        if file.endswith('.pdf'):
+            shutil.move(file, "../files")
