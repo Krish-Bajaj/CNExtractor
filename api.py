ea39177c7aef22975ba38d2a08092f689db03805
Krish Bajaj <53073541+Krish-Bajaj@users.noreply.github.com>
2021-07-16 00:57:52 +0530

    feat: handles data sent through a POST req + CORS enabled

--- api.py
+++ api.py
@@ -2,17 +2,25 @@
 # step by step explanation for setting up and running flask and what each statement means
 
 import flask
-from flask import request, jsonify
+from flask import request, jsonify, abort
+from flask_cors import CORS
+import pdfminer
 from readers.pdf_reader import generateData
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+CORS(app)
 
-@app.route('/contracts/all', methods=['GET'])
+@app.route('/contracts/all', methods=['GET', 'POST'])
 def api_all():
-    data_list = generateData()
-    response = jsonify(data_list)
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+    if request.method == 'POST':
+        try:
+            data_list = generateData(request.get_json()["password"])
+            response = jsonify(data_list) # very important line - otherwise throws a type error
+            return response
+        except pdfminer.pdfdocument.PDFPasswordIncorrect as error:
+            abort(505, description="Password is either not sent or incorrect")
+    # request.form -> get normal data from the endpoint, with request.get_json() u get json data
+    # which gets converted into a python dict
 
 app.run()
