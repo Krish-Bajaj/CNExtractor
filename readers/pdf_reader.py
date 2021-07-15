7ac9fa324fbb4c1d28f1ba70dac0baaf2cdfb442
Krish Bajaj <53073541+Krish-Bajaj@users.noreply.github.com>
2021-07-16 00:58:47 +0530

    feat: enabled password flow

--- readers/pdf_reader.py
+++ readers/pdf_reader.py
@@ -4,29 +4,18 @@ from .fetch_emails import getContractNotes
 import pandas as pd
 import os
 
-def generateData():
+def generateData(password):
     getContractNotes()
 
     # opening a file with 'with' -> .close() doesn't need to be mentioned
     data_list = []
     date_list = []
-    try:
-        file_list = getPDFs('files')
-        for file in file_list:
-            with PDF.open('files/{}'.format(file)) as pdf:
-                data = getData(pdf, date_list)
-                data_list.append(data["data"])
 
-    except:
-        file_list = getPDFs('files')
-        for file in file_list:
-            password = input("PDF is encrypted!\nEnter password: ")
-            try:
-                with PDF.open('files/{}'.format(file), password=password) as pdf:
-                    data = getData(pdf, date_list)
-                    data_list.append(data["data"])
-            except:
-                print("Password entered is incorrect!")
+    file_list = getPDFs('files')
+    for file in file_list:
+        with PDF.open('files/{}'.format(file), password=password) as pdf:
+            data = getData(pdf, date_list)
+            data_list.append(data["data"])
 
     df = pd.DataFrame(data_list)
     # df.to_excel('trades.xlsx')
