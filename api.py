88c321e086cb49516bac24423f17ea15f657a8fe
Krish Bajaj <53073541+Krish-Bajaj@users.noreply.github.com>
2021-07-22 01:11:21 +0530

    feat: connected mongodb to our backend with basic update, retrieve statements

--- api.py
+++ api.py
@@ -5,11 +5,33 @@ import flask
 from flask import request, jsonify, abort
 from flask_cors import CORS
 import pdfminer
+from pymongo import MongoClient
+import datetime
 from readers.pdf_reader import generateData
+import constants
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 CORS(app)
+
+# connects to the database
+client = MongoClient(constants.mongo_url)
+db = client.Cluster1 # connects to the cluster
+users = db.users # creates/accesses the users db
+
+def sendData():
+    key = { "key": "1234567" }
+    userDocument = {
+        "key": "1234567",
+        "name": { "first": "kk", "last": "BBBB" },
+        "birth": datetime.datetime(2001, 6, 1),
+    }
+    users.replace_one(key, userDocument, upsert=True)
+    # "upsert" inserts new data if "key" isn't present otherwise updates current data
+
+    # print(users.find_one({ "name.last": "B" })) # accessing data
+
+# sendData()
 
 @app.route('/contracts/all', methods=['GET', 'POST'])
 def api_all():
